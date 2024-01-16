8cce5040e22d095c56262620872cb29afd99b004
anderthonn <anderthonnluyz@outlook.com>
2024-01-16 14:20:56 -0300

    The database connection context was added to the 'INFRA' layer. In the 'INFRA.IOC' layer, a class was created that extends the 'IServiceCollection' interface to configure the necessary dependency injection for the project.

---
+++ ALB.BLOG.INFRA.IOC/InfrastructureSettings/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using ALB.BLOG.DOMAIN.Models;
+using ALB.BLOG.INFRA.DbContextConnections;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ALB.BLOG.INFRA.IOC.InfrastructureSettings
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+          IConfiguration configuration)
+        {
+            //Database context configuration.
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DbConnectionString"),
+                m => m.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            //Identity configuration.
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
---
+++ ALB.BLOG.INFRA/DbContextConnections/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ALB.BLOG.DOMAIN.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ALB.BLOG.INFRA.DbContextConnections
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ApplicationUser>? ApplicationUsers { get; set; }
+        public DbSet<Email>? Emails { get; set; }
+        public DbSet<Page>? Pages { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<Setting>? Settings { get; set; }
+    }
+}
